15225303079c9d781cec130c198f658f59dcaf6b
Alex-Fyodorov <rassylka82@inbox.ru>
2021-07-28 16:49:26 +0300

    lesson1 повтор

---
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -0,0 +1,63 @@
+package lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        System.out.println("Задание 2:");
+        printThreeWords();
+        System.out.println();
+        System.out.println("Задание 3:");
+        checkSumSign();
+        System.out.println();
+        System.out.println("Задание 4:");
+        printColor();
+        System.out.println();
+        System.out.println("Задание 5:");
+        compareNumbers();
+
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 10;
+        int b = -20;
+        System.out.println("a = " + a + ", b = " + b);
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int a = 10;
+        System.out.println("a = " + a);
+        if (a <= 0) {
+            System.out.println("Красный");
+        } else if (a > 0 && a <= 100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+        /* Я понимаю, что можно было сделать проще, без логического
+        оператора "И", но сделал так больше для того, чтобы показать,
+        что знаю и умею.
+         */
+    }
+
+    public static void compareNumbers() {
+        int a = 125;
+        int b = 478;
+        System.out.println("a = " + a + ", b = " + b);
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
