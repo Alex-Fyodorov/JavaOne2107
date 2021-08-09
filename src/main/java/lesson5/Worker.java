11f01c03d91c3ea5f777eba349e55b53890b58b2
Alex Fyodorov <rassylka82@inbox.ru>
2021-08-09 16:27:01 +0300

    lesson5

---
+++ src/main/java/lesson5/Worker.java
@@ -0,0 +1,40 @@
+package lesson5;
+
+public class Worker {
+    private String name;
+    private String post;
+    private String e_mail;
+    private long phone;
+    private int salary;
+    private int age;
+
+    public Worker(String name, String post, String e_mail,
+                  long phone, int salary, int age) {
+        this.name = name;
+        this.post = post;
+        this.e_mail = e_mail;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void info() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", post='" + post + '\'' +
+                ", e_mail='" + e_mail + '\'' +
+                ", phone=" + phone +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+}
