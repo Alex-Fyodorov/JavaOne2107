54f023f2762929d99a78febc9f216d7c9e8f1837
Alex Fyodorov <rassylka82@inbox.ru>
2021-08-15 16:09:57 +0300

    lesson6

---
+++ src/main/java/lesson6/Animal.java
@@ -0,0 +1,25 @@
+package lesson6;
+
+public abstract class Animal {
+    protected String name;
+    protected int runDistance;
+    protected int swimDistance;
+    protected static int count;
+
+    public Animal() {
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", runDistance=" + runDistance +
+                ", swimDistance=" + swimDistance +
+                '}';
+    }
+}
