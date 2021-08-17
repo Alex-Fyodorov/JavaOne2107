624d64440189d23f27694f0ee8346cb992dc69d5
Alex Fyodorov <rassylka82@inbox.ru>
2021-08-17 19:28:28 +0300

    lesson6

--- src/main/java/lesson1/ExperimentApp.java
+++ src/main/java/lesson1/ExperimentApp.java
@@ -28,7 +28,53 @@ public class ExperimentApp {
 
         // 4. Объединить массив в строку.
         String ss = new String(aaa);
-        System.out.println(ss);
+        System.out.println("4. " + ss);
+
+        //5. Получение длины строки.
+        int ddd = sss.length();
+        System.out.println("5. " + ddd);
+
+        //6. Извлечение из строки символа, находящегося на какой-либо позиции.
+        System.out.println("6. " + sss.charAt(2));
+
+        //7. Посимвольное сравнение строк.
+        String www = "SKDFJHskjh";
+        System.out.println("7. " + www.equalsIgnoreCase(sss));
+
+        //8. Объединение двух строк в одну. То же действие, что и "+".
+        System.out.println("8. " + www.concat(sss));
+
+        //9. Изменение регистра в строке.
+        System.out.println("9. " + www.toLowerCase());
+        System.out.println("9. " + www.toUpperCase());
+
+        //10. Преобразование в строку.
+        int xx = 123456789;
+        String ff = String.valueOf(xx);
+        System.out.println("10. " + ff);
+
+        //11. Методы StringBuilder и StringBuffer
+        StringBuilder sb = new StringBuilder();
+        StringBuffer stringBuffer = new StringBuffer();
+
+        // Добавление элементов.
+        sb.append("s1");
+        sb.append("s2");
+        sb.append("a1").append(10).append("a2").append(true);
+
+        System.out.println(sb);
+        // Объём выделенной памяти.
+        System.out.println(sb.capacity());
+
+        // Установка длины строки.
+        sb.setLength(5);
+        System.out.println(sb);
+        sb.setLength(10);
+        System.out.println(sb);
+
+        String stringFromSB = sb.toString();
+
+
 
     }
 }
